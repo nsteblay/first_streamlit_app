b4e40a0c487d03840de6fd8ae22329022132b0a5
nsteblay <nsteblay@gmail.com>
2022-04-24 13:17:22 -0500

    Another

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import streamlit
 streamlit.title('My Mom\'s New Super Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -9,5 +10,5 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Something'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),default=['Avacado','Stawberries'])
 streamlit.dataframe(my_fruit_list)
