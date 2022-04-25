d6d4963b910ec4a71e51d1d2c6e331028f8d821c
nsteblay <nsteblay@gmail.com>
2022-04-25 17:56:25 -0500

    Again

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,8 @@ streamlit.dataframe(fruits_to_show)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+# Normalize response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output to screen
+streamlit.dataframe(fruityvice_normalized)
+
