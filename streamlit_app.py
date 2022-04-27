9f4ec3b936b2f16d4f0c1ca8bad4dcbcf2981491
nsteblay <nsteblay@gmail.com>
2022-04-27 05:31:45 -0500

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
-# from email.policy import default
+from email.policy import default
 import streamlit
 import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
+
 streamlit.title('My Mom\'s New Super Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -17,17 +18,20 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
 # New section
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json())
-# Normalize response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output to screen
-streamlit.dataframe(fruityvice_normalized)
 # --------
 streamlit.stop()
 
